f349e34d129f99fd91c16ff083ed861735124316
Ilya Gaifullin <proskmax@yandex.ru>
2025-06-16 19:48:51 +0500

    ci/cd(12)

--- config/settings.py
+++ config/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1", "localhost", "host.docker.internal"]
 
 
 # Application definition
@@ -172,11 +172,12 @@ SPECTACULAR_SETTINGS = {
 }
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / "static"]
 
 #STATICFILES_DIRS=[BASE_DIR/'static']
 
 MEDIA_DIR = '/media/'
-MEDIA_ROOT = BASE_DIR/ 'media'
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
@@ -192,10 +193,10 @@ STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY')
 # Настройки для Celery
 
 # URL-адрес брокера сообщений
-CELERY_BROKER_URL = 'redis://localhost:6379' # Например, Redis, который по умолчанию работает на порту 6379
+CELERY_BROKER_URL = 'redis://redis:6379' # Например, Redis, который по умолчанию работает на порту 6379
 
 # URL-адрес брокера результатов, также Redis
-CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+CELERY_RESULT_BACKEND = 'redis://redis:6379'
 
 # Часовой пояс для работы Celery
 CELERY_TIMEZONE = "Australia/Tasmania"
@@ -213,3 +214,12 @@ CELERY_BEAT_SCHEDULE = {
         "schedule": timedelta(weeks=4),  # Расписание выполнения задачи
     }
 }
+
+CACHE_ENABLED = True
+if CACHE_ENABLED:
+    CACHES = {
+        "default": {
+            "BACKEND": "django.core.cache.backends.redis.RedisCache",
+            "LOCATION": "redis://redis:6379/1",
+        }
+    }
